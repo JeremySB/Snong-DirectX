62af9b5f497b39d6e056e0aed226c2e02d29b01c
Jeremy Bost <bostjs1@gcc.edu>
2016-10-14 11:39:46 -0400

    Quickly resolving merge conflict

--- Snong.cpp
+++ Snong.cpp
@@ -31,10 +31,10 @@ Snong::~Snong()
 void Snong::initialize(HWND hwnd)
 {
     Game::initialize(hwnd); // throws GameError
-//<<<<<<< HEAD
+
 	Player1.initialize(this, BOARD_WIDTH/3, BOARD_HEIGHT/2, P1_SNAKE_HEAD_TEXTURE, P1_SNAKE_LINK_TEXTURE);
 	Player2.initialize(this, (BOARD_WIDTH/3) * 2, BOARD_HEIGHT/2, P2_SNAKE_HEAD_TEXTURE, P2_SNAKE_LINK_TEXTURE);
-//=======
+
 	
 	// background initializations
 	if(!backgroundTexture.initialize(graphics, BACKGROUND_IMAGE)) 
@@ -59,11 +59,6 @@ void Snong::initialize(HWND hwnd)
 			GameError(gameErrorNS::FATAL_ERROR, "Border image initialization failed");
 	borderImage2.setX(GAME_WIDTH/2 + WIDTH_BETWEEN_ARENAS/2);
 	borderImage2.setY(0);
-
-	
-//	Player1.initialize(this, 0, 0, P1_SNAKE_HEAD_TEXTURE, P1_SNAKE_LINK_TEXTURE);
-//	Player2.initialize(this, 0, 0, P2_SNAKE_HEAD_TEXTURE, P2_SNAKE_LINK_TEXTURE);
-//>>>>>>> 1e4d4437d759a1a699402bf67111bdfc989a1f75
 
 	// ball initializations
 	if(!ballTexture.initialize(graphics, BALL_IMAGE)) 
